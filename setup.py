cb0db626c37e562f6d311ef5dd014d7ad1d8a7d8
Carldeboer <carlgdeboer@gmail.com>
2018-02-26 17:17:32 -0500

    testing mod to setup.py to fix install issues

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
      name='CisRegModels',    # This is the name of your PyPI-package.
      version='1.0',                          # Update the version number for new releases
      description='Various scripts for processing GPRA data, as well as making models of cis-regulatory logic from GPRA data.',
+     packages=find_packages(),
      url='https://github.com/Carldeboer/CisRegModels',
      install_requires=['tensorflow','numpy','datetime'],
      scripts=['collapsePromoters.py','mergeSeqsByBowtie.py','seqsToOHC.py','translateSequencesByDict.py','alignFastqsIntoSeqs.py','makeThermodynamicEnhancosomeModel.py', 'predictThermodynamicEnhancosomeModel.py']                  # The name of your scipt, and also the command you'll be using for calling it
